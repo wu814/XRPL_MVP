3179ac7419b36cb28ef39b636bb86af359d9d624
wu814 <william14.wu@gmail.com>
2026-04-25 22:36:51 -0500

    fix error

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,10 +1,10 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-import type { Linter } from 'eslint';
+import type { Linter } from "eslint";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(import.meta.url);
+const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
